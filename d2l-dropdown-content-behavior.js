f833efc51842eea72886cd2920d78badfa69697a
Dave Batiste <dave.batiste@d2l.com>
2019-03-04 11:48:07 -0500

    Update z-index of top/bottom caps so that tabs scroll buttons will be underneath.

--- d2l-dropdown-content-behavior.js
+++ d2l-dropdown-content-behavior.js
@@ -110,7 +110,7 @@ $_documentContainer.innerHTML = `<dom-module id="d2l-dropdown-content-styles">
 			:host .d2l-dropdown-content-bottom {
 				height: 5px;
 				position: relative;
-				z-index: 1;
+				z-index: 2;
 			}
 
 			:host .d2l-dropdown-content-top {
